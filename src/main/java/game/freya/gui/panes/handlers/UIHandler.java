173e6b4499718eb212b11c8584b66a54a68ecc74
KiraLis39 <k.romanenko@notamedia.com.ru>
2023-11-05 14:25:05 +0300

    Доработка игрового UI

--- src/main/java/game/freya/gui/panes/handlers/UIHandler.java
+++ src/main/java/game/freya/gui/panes/handlers/UIHandler.java
@@ -14,12 +14,24 @@ public final class UIHandler {
     }
 
     public static void drawUI(Rectangle canvasRect, Graphics2D g2D) {
+        // up left pane:
         g2D.setStroke(new BasicStroke(2f));
         g2D.setColor(Color.GREEN);
         g2D.drawRect(1, 1, (int) (canvasRect.getWidth() * 0.333f), (int) (canvasRect.getHeight() * 0.075f));
 
+        // up center pane:
+        g2D.setColor(Color.YELLOW);
+        g2D.drawRect((int) (canvasRect.getWidth() * 0.36f), 1,
+                (int) (canvasRect.getWidth() * 0.28f), (int) (canvasRect.getHeight() * 0.075f));
+
+        // up right pane:
         g2D.setColor(Color.MAGENTA);
         g2D.drawRect((int) (canvasRect.getWidth() * 0.666f), 1,
                 (int) (canvasRect.getWidth() * 0.333f), (int) (canvasRect.getHeight() * 0.075f));
+
+        // down center pane:
+        g2D.setColor(Color.YELLOW);
+        g2D.drawRect((int) (canvasRect.getWidth() * 0.36f), (int) (canvasRect.getHeight() * 0.925f),
+                (int) (canvasRect.getWidth() * 0.28f), (int) (canvasRect.getHeight() * 0.075f));
     }
 }
