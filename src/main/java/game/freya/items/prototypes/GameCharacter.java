f26e516aa8e86545ba254f7bb3a23502c8c913b7
KiraLis39 <k.romanenko@notamedia.com.ru>
2023-12-06 23:46:00 +0300

    Версия 0.1.4 Протестировано в он-лайн тремя людьми одновременно. Игра достаточно стабильна даже через Радмин. Можно готовиться к переходу на GLCanvas.

--- src/main/java/game/freya/items/prototypes/GameCharacter.java
+++ src/main/java/game/freya/items/prototypes/GameCharacter.java
@@ -1,7 +1,6 @@
 package game.freya.items.prototypes;
 
 import game.freya.interfaces.iGameObject;
-import game.freya.interfaces.iHero;
 
-public abstract class GameCharacter implements iGameObject, iHero {
+public abstract class GameCharacter implements iGameObject {
 }
